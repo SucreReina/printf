b5e8845ce7a1cf20e49d39a520b8059de387588f
Rheedn <algoy6jacob@gmail.com>
2023-07-25 22:08:24 +0100

    printf

--- functions1.c
+++ functions1.c
@@ -97,15 +97,16 @@ int print_hexadecimal(va_list types, char buffer[],
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
- *  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation*
+ * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
  * @types: Lista of arguments
- *    * @buffer: Buffer array to handle print
- *     * @flags:  Calculates active flags
- *      * @width: get width
- *       * @precision: Precision specification
- *        * @size: Size specifier
- *         * Return: Number of chars printed
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
+
 int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size)
 {
